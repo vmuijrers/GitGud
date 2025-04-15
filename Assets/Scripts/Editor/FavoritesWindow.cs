6ed259c316db93ae1892d8410280d7ef623de0b6
Valentijn <vmuijrers@gmail.com>
2025-04-15 22:54:19 +0200

    Added Categories to the Favorite window

--- Assets/Scripts/Editor/FavoritesWindow.cs
+++ Assets/Scripts/Editor/FavoritesWindow.cs
@@ -1,11 +1,14 @@
 using UnityEditor;
 using UnityEngine;
 using System.IO;
-
+using System.Collections.Generic;
+using System.Reflection;
 public class FavoritesWindow : EditorWindow
 {
-    private ResourceCollection collection;
+    public ResourceCollection resourceCollection;
     private Vector2 scroll;
+
+    private string newCategoryName = "New Category";
 
     [MenuItem("Window/Favorites")]
     public static void ShowWindow()
@@ -15,46 +18,81 @@ public class FavoritesWindow : EditorWindow
 
     private void OnGUI()
     {
-        EditorGUILayout.LabelField("Favorite Assets", EditorStyles.boldLabel);
-        collection = EditorGUILayout.ObjectField(collection, typeof(ResourceCollection), false) as ResourceCollection;
-        EditorGUILayout.Space();
+        resourceCollection = EditorGUILayout.ObjectField(resourceCollection, typeof(ResourceCollection), false) as ResourceCollection;
+        if (resourceCollection == null) { return; }
 
-        if(collection != null && collection.favorites != null)
+        scroll = EditorGUILayout.BeginScrollView(scroll);
+        List<ResourceList> categories = new List<ResourceList>(resourceCollection.resources);
+        foreach (ResourceList category in categories)
         {
-            scroll = EditorGUILayout.BeginScrollView(scroll);
-
-            for (int i = 0; i < collection.favorites.Count; i++)
+            
+            EditorGUILayout.BeginVertical("box");
+            GUILayout.BeginHorizontal();
+            category.isFoldout = EditorGUILayout.Foldout(category.isFoldout, category.name, true);
+            GUILayout.FlexibleSpace(); // Pushes the X to the right
+            if (GUILayout.Button("X"))
             {
-                EditorGUILayout.BeginHorizontal();
+                resourceCollection.resources.Remove(category);
+                GUILayout.EndHorizontal();
+                EditorGUILayout.EndVertical();
+                break;
+            }
+            GUILayout.EndHorizontal();
+            Rect foldoutRect = GUILayoutUtility.GetLastRect();
+            HandleCategoryDrop(category.name, foldoutRect);
 
-                collection.favorites[i] = EditorGUILayout.ObjectField(collection.favorites[i], typeof(Object), false);
-
-                if (GUILayout.Button("Ping", GUILayout.Width(40)))
+            if (category.isFoldout)
+            {
+                List<Object> favorites = category.favorites;
+                for (int i = 0; i < favorites.Count; i++)
                 {
-                    EditorGUIUtility.PingObject(collection.favorites[i]);
-                }
+                    EditorGUILayout.BeginHorizontal();
 
-                if (GUILayout.Button("X", GUILayout.Width(20)))
-                {
-                    collection.favorites.RemoveAt(i);
-                    break;
-                }
+                    favorites[i] = EditorGUILayout.ObjectField(favorites[i], typeof(Object), false);
 
-                EditorGUILayout.EndHorizontal();
+                    if (GUILayout.Button("X", GUILayout.Width(20)))
+                    {
+                        favorites.RemoveAt(i);
+                        break;
+                    }
+
+                    EditorGUILayout.EndHorizontal();
+                }
             }
 
-            EditorGUILayout.EndScrollView();
-
-            EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Drag assets here to add them:");
-            Rect dropArea = GUILayoutUtility.GetRect(0.0f, 50.0f, GUILayout.ExpandWidth(true));
-            GUI.Box(dropArea, "Drop here");
-
-            HandleDragAndDrop(dropArea);
+            EditorGUILayout.EndVertical();
         }
+
+        EditorGUILayout.EndScrollView();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField($"Drag assets here to add them (adds to '{newCategoryName}'):");
+        Rect dropArea = GUILayoutUtility.GetRect(0.0f, 50.0f, GUILayout.ExpandWidth(true));
+        GUI.Box(dropArea, "Drop here");
+
+        HandleDragAndDrop(dropArea, newCategoryName);
+
+        EditorGUILayout.Space();
+        DrawCategoryCreationUI();
     }
 
-    private void HandleDragAndDrop(Rect dropArea)
+    private void DrawCategoryCreationUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+        newCategoryName = EditorGUILayout.TextField(newCategoryName);
+
+        if (GUILayout.Button("Add Category"))
+        {
+            if(resourceCollection.GetResourceListByCategory(newCategoryName) == null)
+            {
+                resourceCollection.AddCategory(newCategoryName);
+            }
+        }
+
+        EditorGUILayout.EndHorizontal();
+    }
+
+    private void HandleDragAndDrop(Rect dropArea, string targetCategory)
     {
         Event evt = Event.current;
         if ((evt.type == EventType.DragUpdated || evt.type == EventType.DragPerform) && dropArea.Contains(evt.mousePosition))
@@ -65,38 +103,122 @@ public class FavoritesWindow : EditorWindow
             {
                 DragAndDrop.AcceptDrag();
 
+                if(!resourceCollection.resources.Exists(x=>x.name == targetCategory))
+                {
+                    resourceCollection.AddCategory(targetCategory);
+                }
+
                 foreach (Object draggedObject in DragAndDrop.objectReferences)
                 {
                     string path = AssetDatabase.GetAssetPath(draggedObject);
-
+                    
                     if (AssetDatabase.IsValidFolder(path))
                     {
-                        // Get all asset paths inside the folder (non-recursive or recursive)
                         string[] assetGUIDs = AssetDatabase.FindAssets("", new[] { path });
+                        //ShowFolderContents(draggedObject.GetInstanceID());
 
                         foreach (string guid in assetGUIDs)
                         {
                             string assetPath = AssetDatabase.GUIDToAssetPath(guid);
                             Object asset = AssetDatabase.LoadAssetAtPath<Object>(assetPath);
 
-                            if (asset != null && !collection.favorites.Contains(asset))
+                            if (asset != null && !resourceCollection.GetResourceListByCategory(targetCategory).favorites.Contains(asset))
                             {
-                                collection.favorites.Add(asset);
+                                resourceCollection.GetResourceListByCategory(targetCategory).favorites.Add(asset);
                             }
                         }
                     }
                     else
                     {
-                        // Regular asset
-                        if (!collection.favorites.Contains(draggedObject))
+                        if (!resourceCollection.GetResourceListByCategory(targetCategory).favorites.Contains(draggedObject))
                         {
-                            collection.favorites.Add(draggedObject);
+                            resourceCollection.GetResourceListByCategory(targetCategory).favorites.Add(draggedObject);
                         }
                     }
                 }
             }
 
-            Event.current.Use();
+            evt.Use();
         }
+    }
+
+    private void HandleCategoryDrop(string category, Rect categoryRect)
+    {
+        Event evt = Event.current;
+        if ((evt.type == EventType.DragUpdated || evt.type == EventType.DragPerform) && categoryRect.Contains(evt.mousePosition))
+        {
+            DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+
+            if (evt.type == EventType.DragPerform)
+            {
+                DragAndDrop.AcceptDrag();
+                HandleDragAndDrop(categoryRect, category);
+            }
+
+            evt.Use();
+        }
+    }
+
+    /// <summary>
+    /// Selects a folder in the project window and shows its content.
+    /// Opens a new project window, if none is open yet.
+    /// </summary>
+    /// <param name="folderInstanceID">The instance of the folder asset to open.</param>
+    private static void ShowFolderContents(int folderInstanceID)
+    {
+        // Find the internal ProjectBrowser class in the editor assembly.
+        Assembly editorAssembly = typeof(Editor).Assembly;
+        System.Type projectBrowserType = editorAssembly.GetType("UnityEditor.ProjectBrowser");
+
+        // This is the internal method, which performs the desired action.
+        // Should only be called if the project window is in two column mode.
+        MethodInfo showFolderContents = projectBrowserType.GetMethod(
+            "ShowFolderContents", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        // Find any open project browser windows.
+        Object[] projectBrowserInstances = Resources.FindObjectsOfTypeAll(projectBrowserType);
+
+        if (projectBrowserInstances.Length > 0)
+        {
+            for (int i = 0; i < projectBrowserInstances.Length; i++)
+                ShowFolderContentsInternal(projectBrowserInstances[i], showFolderContents, folderInstanceID);
+        }
+        else
+        {
+            EditorWindow projectBrowser = OpenNewProjectBrowser(projectBrowserType);
+            ShowFolderContentsInternal(projectBrowser, showFolderContents, folderInstanceID);
+        }
+    }
+
+    private static void ShowFolderContentsInternal(Object projectBrowser, MethodInfo showFolderContents, int folderInstanceID)
+    {
+        // Sadly, there is no method to check for the view mode.
+        // We can use the serialized object to find the private property.
+        SerializedObject serializedObject = new SerializedObject(projectBrowser);
+        bool inTwoColumnMode = serializedObject.FindProperty("m_ViewMode").enumValueIndex == 1;
+
+        if (!inTwoColumnMode)
+        {
+            // If the browser is not in two column mode, we must set it to show the folder contents.
+            MethodInfo setTwoColumns = projectBrowser.GetType().GetMethod(
+                "SetTwoColumns", BindingFlags.Instance | BindingFlags.NonPublic);
+            setTwoColumns.Invoke(projectBrowser, null);
+        }
+
+        bool revealAndFrameInFolderTree = true;
+        showFolderContents.Invoke(projectBrowser, new object[] { folderInstanceID, revealAndFrameInFolderTree });
+    }
+
+    private static EditorWindow OpenNewProjectBrowser(System.Type projectBrowserType)
+    {
+        EditorWindow projectBrowser = EditorWindow.GetWindow(projectBrowserType);
+        projectBrowser.Show();
+
+        // Unity does some special initialization logic, which we must call,
+        // before we can use the ShowFolderContents method (else we get a NullReferenceException).
+        MethodInfo init = projectBrowserType.GetMethod("Init", BindingFlags.Instance | BindingFlags.Public);
+        init.Invoke(projectBrowser, null);
+
+        return projectBrowser;
     }
 }
--- Assets/Scripts/Editor/ResourceCollection.cs
+++ Assets/Scripts/Editor/ResourceCollection.cs
@@ -1,8 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 [CreateAssetMenu]
 public class ResourceCollection : ScriptableObject
 {
+    public List<ResourceList> resources = new List<ResourceList>();
+
+    public ResourceList GetResourceListByCategory(string category)
+    {
+        return resources.Find(x=>x.name == category);
+    }
+
+    public List<string> GetCategories()
+    {
+        return resources.Select(x=>x.name).ToList();
+    }
+
+    public void AddCategory(string category)
+    {
+        resources.Add(new ResourceList()
+        {
+            name = category,
+            favorites = new List<Object>()
+        });
+    }
+}
+
+[System.Serializable]
+public class ResourceList
+{
+    public string name;
+    public bool isFoldout = true;
     public List<Object> favorites = new List<Object>();
 }
